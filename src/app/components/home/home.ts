ea45ffcaad4f81a57029c1d8a34fdf59797a452e
Johan <johan.rivet@hotmail.com>
2025-07-15 22:06:07 +0200

    Mise à jour page home avec styles et fonctionnalité Instagram

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,24 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; // pour routerLink
 import {
   FontAwesomeModule,
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { RouterModule } from '@angular/router'; // <-- important pour routerLink
+import { faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterModule], // <-- ajoute RouterModule ici
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
 export class HomeComponent {
   constructor(library: FaIconLibrary) {
-    library.addIcons(faInstagram);
+    library.addIcons(faInstagram, faGithub);
   }
 
   openInstagram(): void {
     window.open('https://www.instagram.com/ngreact0/', '_blank');
+  }
+
+  openGithub(): void {
+    window.open('https://github.com/R-Johan', '_blank');
   }
 }
